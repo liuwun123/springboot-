423e01dd2764d75b3d3346cf846839bcaab6f7f6
lx <568875635@qq.com>
2018-03-13 21:11:43 +0800

    springboot基本的属性配置

--- src/main/java/com/liuxun/demo/HelloWord.java
+++ src/main/java/com/liuxun/demo/HelloWord.java
@@ -1,13 +1,32 @@
 package com.liuxun.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.liuxun.demo.properties.GirlProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWord {
-    @RequestMapping(value = "/helloBoot",method = RequestMethod.GET)
-    public String sayHellWord(){
-            return "初次学习SpringBoot";
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    /*@RequestMapping(value = {"/say","/hi"},method = RequestMethod.GET)*/
+    @GetMapping(value="/say")
+    public Map<String, Object> sayHellWord(@RequestParam(value = "id",required = false,defaultValue = "01")  Long id){
+        Map<String,Object> jsonMap = new HashMap<>();
+            if(id==1l ){
+                jsonMap.put("code",602);
+                jsonMap.put("msg","id为默认值");
+            }else {
+                jsonMap.put("code", 601);
+                jsonMap.put("msg", "id已传:" + id);
+            }
+            return jsonMap;
         }
 }
---
+++ src/main/java/com/liuxun/demo/properties/GirlProperties.java
@@ -0,0 +1,29 @@
+package com.liuxun.demo.properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+    private String cupSize;
+    private Integer age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
